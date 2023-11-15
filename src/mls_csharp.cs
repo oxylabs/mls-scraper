a55911dfa50af61ac06f4742e209fd9660a306dc
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-11-15 14:13:43 +0200

    Create mls_csharp.cs

---
+++ src/mls_csharp.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace OxyApi
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            const string Username = "USERNAME";
+            const string Password = "PASSWORD";
+
+            var parameters = new Dictionary&lt;string, string>()
+            {
+		{"source", "universal"},
+		{"url", "https://mls.foreclosure.com/listing/search.html?ci=bay%20shore&st=ny&utm_source=internal&utm_medium=link&utm_campaign=MLS_top_links"}
+            };
+
+            var client = new HttpClient();
+
+            Uri baseUri = new Uri("https://realtime.oxylabs.io");
+            client.BaseAddress = baseUri;
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Post, "/v1/queries");
+            requestMessage.Content = JsonContent.Create(parameters);
+
+            var authenticationString = $"{Username}:{Password}";
+            var base64EncodedAuthenticationString = Convert.ToBase64String(System.Text.ASCIIEncoding.UTF8.GetBytes(authenticationString));
+            requestMessage.Headers.Add("Authorization", "Basic " + base64EncodedAuthenticationString);
+
+            var response = await client.SendAsync(requestMessage);
+            var contents = await response.Content.ReadAsStringAsync();
+
+            Console.WriteLine(contents);
+        }
+    }
+}
